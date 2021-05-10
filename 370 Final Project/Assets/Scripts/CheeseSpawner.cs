2ff2acb5de50af7921270689b5e8213eeee36440
Nate12503 <nathanshussey@gmail.com>
2021-05-09 22:09:17 -0400

    Basic Random Cheese spawning

---
+++ 370 Final Project/Assets/Scripts/CheeseSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheeseSpawner : MonoBehaviour
+{
+    public GameObject[] pickups;
+    private int randomCheese;
+
+    private void Start()
+    {
+        SpawnNewCheese();
+    }
+    public void SpawnNewCheese()
+    {
+        randomCheese = (int)(Random.Range(0, pickups.Length) + .5f);
+        pickups[randomCheese].SetActive(true);
+    }
+}
